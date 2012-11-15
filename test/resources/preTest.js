38d94dbb367d4557ee8a645ddfcac5aeae63ef27
Daniel Lehr <Daniellehr@gmx.de>
2012-11-15 23:41:25 +0100

    added unit tests (no full coverage yet)

---
+++ test/resources/preTest.js
@@ -0,0 +1,75 @@
+// just throw some table together
+function createTestTable(id, cols, rows, prefix) {
+  prefix = prefix || 'sort';
+
+  var table = '<table id="'+id+'" class="plugin-test">';
+  table += '<thead>';
+  table += '<tr>';
+  var dataLen = 0;
+
+  $.each(cols, function(sort, name) {
+    ++dataLen;
+    table += '<th class="'+prefix+'-'+sort+'">'+name+'</th>';
+  });
+  table += '</tr>';
+  table += '</thead>';
+
+  table += '<tbody>';
+  $.each(rows, function(i, row) {
+    if (row.length == dataLen) {
+      table += '<tr>';
+      $.each(row, function(i, value) {
+        table += '<td>'+value+'</td>';
+      });
+      table += '</tr>';
+    }
+  });
+  table += '</tbody>';
+  table += '</table>';
+
+  $('body').after(table);
+  return $('#'+id);
+}
+
+var testToolkit = {
+  hasSameOrder: function(expected, actual) {
+    var result = true;
+    $.each(expected, function(i, val) {
+      if (val !== actual[i]) {
+        result = false;
+        return false;
+      }
+    });
+    return result;
+  },
+
+  getExpectedData: function(testData, column, orderBy) {
+    if (!this.sortMethods[orderBy]) {
+      throw "Sort method not found";
+    }
+
+    var expectedData = [];
+    $.each(testData, function(i, val) {
+      expectedData.push(val[column]);
+    });
+    return expectedData.sort(this.sortMethods[orderBy]);
+  },
+
+  sortMethods: {
+    numeric: function(a, b) {
+      return (parseInt(a) - parseInt(b));
+    },
+    float: function(a, b) {
+      return (parseFloat(a) - parseFloat(b));
+    },
+    alphabetical: function(a, b) {
+      return (a.toLowerCase() > b.toLowerCase()) ? 1 : -1;
+    },
+    date: function(a, b) {
+      a = new Date(a);
+      b = new Date(b);
+
+      return (a > b) ? 1 : -1;
+    }
+  }
+};
